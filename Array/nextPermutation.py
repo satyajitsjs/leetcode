745cad4aac5a9f86f288a1fbd1d197b2d4548667
satyajitsjs <satyajitsjs3@gmail.com>
2024-06-17 12:58:32 +0530

    nextPermutaion

---
+++ Array/nextPermutation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nextPermutation(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+
+
+
+nums = [1,2,3]
+s = Solution()
+print(s.nextPermutation(nums))
